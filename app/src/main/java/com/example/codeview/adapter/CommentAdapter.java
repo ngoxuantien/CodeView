19fa904320f78eb2a5e39084c09d5945be414ebe
ngoxuantien <55541378+ngoxuantien@users.noreply.github.com>
2021-06-04 14:12:33 +0700

    v7

--- app/src/main/java/com/example/codeview/adapter/CommentAdapter.java
+++ app/src/main/java/com/example/codeview/adapter/CommentAdapter.java
@@ -9,12 +9,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.codeview.R;
+import com.example.codeview.databinding.ItemCommentBinding;
 import com.example.codeview.model.Comment;
+import com.example.codeview.util.CustomClickListener;
 
 import java.util.List;
 
@@ -31,51 +34,43 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
     @NonNull
     @Override
     public CommentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_comment, parent, false));
+        ItemCommentBinding itemCommentBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_comment, parent, false);
+
+        return new ViewHolder(itemCommentBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentAdapter.ViewHolder holder, int position) {
+        final Comment comment = commentList.get(position);
+        final boolean[] h = {true};
+        holder.itemCommentBinding.setComment(comment);
+        holder.bind(comment);
 
-        final int[] i = {1};
-        //  Glide.with(context).load(commentList.get(position).getCommentImage()).into(holder.commentImage);
-        holder.commentName.setText(commentList.get(position).getCommentName());
-        holder.commentTime.setText(commentList.get(position).getCommentTime());
-        holder.commentContent.setText(commentList.get(position).getCommentContent());
-        holder.commentLike.setText(commentList.get(position).getCommentLike() + "");
-        holder.collapse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (i[0] == 1) {
-                    holder.recyclerViewResponse.setVisibility(View.GONE);
-                    i[0] = 2;
-                } else {
-                    holder.recyclerViewResponse.setVisibility(View.VISIBLE);
-                    i[0] = 1;
-                }
-
+        // set cuộn ra cuộn vào
+        holder.itemCommentBinding.setClickCollape(() -> {
+            if (h[0]) {
+                holder.itemCommentBinding.setRoll(true);
+                h[0] = false;
+            } else {
+                holder.itemCommentBinding.setRoll(false);
+                h[0] = true;
             }
         });
-        if (commentList.get(position).getReponsList() != null) {
 
-            setListCommentResponseRecyclervew(holder.recyclerViewResponse, commentList.get(position).getReponsList());
-            holder.collapse.setText("Ver respuestas (" + commentList.get(position).getReponsList().size() + ")");
+
+        if (commentList.get(position).getReponsList() != null) {
+            holder.itemCommentBinding.setView(true);
+            // xét recyclerview
+            holder.itemCommentBinding.setAdapter( new CommentResponseAdapter(context,commentList.get(position).getReponsList()));
+            // set test số bình luận của list
+            holder.itemCommentBinding.setNumberOfComment("Ver respuestas (" + commentList.get(position).getReponsList().size() + ")");
         } else {
-            holder.collapse.setVisibility(View.GONE);
-            holder.itemCollapse.setVisibility(View.GONE);
+            holder.itemCommentBinding.setView(false);
+
         }
 
-        // đưa id của comment cha lên sesson
-        holder.commentResponse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences sharedPreferences = view.getContext().getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("commentId",commentList.get(position).getCommentId()+"");
-                editor.commit();
-            }
-        });
+
+
 
     }
 
@@ -84,32 +79,24 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
         return commentList.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
-        RecyclerView recyclerViewResponse;
-        ImageView commentImage, itemCollapse;
-        TextView commentName, commentContent, commentTime, commentLike, collapse, commentResponse;
+        private ItemCommentBinding itemCommentBinding;
+     //   RecyclerView recyclerViewResponse;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            recyclerViewResponse = itemView.findViewById(R.id.recyclerviewResponse);
-            commentName = itemView.findViewById(R.id.commentName);
-            collapse = itemView.findViewById(R.id.collapse);
-            commentImage = itemView.findViewById(R.id.commentimage);
-            commentContent = itemView.findViewById(R.id.commentContent);
-            commentTime = itemView.findViewById(R.id.commentTime);
-            commentLike = itemView.findViewById(R.id.commentLike);
-            itemCollapse = itemView.findViewById(R.id.iconCollapse);
-            commentResponse = itemView.findViewById(R.id.commentResponse);
+        public ViewHolder(@NonNull ItemCommentBinding binding) {
+            super(binding.getRoot());
+            this.itemCommentBinding = binding;
 
+        }
+
+        //// cần tìm hiểu thêm///
+        public void bind(Object obj) {
+            itemCommentBinding.executePendingBindings();
         }
 
     }
 
-    public void setListCommentResponseRecyclervew(RecyclerView recyclervew, List<Comment> commentList) {
-        recyclervew.setNestedScrollingEnabled(false);
-        CommentResponseAdapter commentResponseAdapter = new CommentResponseAdapter(context, commentList);
-        recyclervew.setLayoutManager(new LinearLayoutManager(context, RecyclerView.VERTICAL, false));
-        recyclervew.setAdapter(commentResponseAdapter);
-    }
+
 
 }
--- app/src/main/java/com/example/codeview/util/CustomClickListener.java
+++ app/src/main/java/com/example/codeview/util/CustomClickListener.java
@@ -1,6 +1,10 @@
 package com.example.codeview.util;
 
+import com.example.codeview.model.Comment;
+
 public interface CustomClickListener {
+ void setClickCollapse();
+
 
 
 }
