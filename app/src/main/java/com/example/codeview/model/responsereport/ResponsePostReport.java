3f99b33ef66e26b48b2df90e4fceb979cde47588
ngoxuantien <55541378+ngoxuantien@users.noreply.github.com>
2021-06-29 09:00:24 +0700

    hoan thien

--- app/src/main/java/com/example/codeview/model/responsereport/ResponsePostReport.java
+++ app/src/main/java/com/example/codeview/model/responsereport/ResponsePostReport.java
@@ -1,4 +1,45 @@
+
 package com.example.codeview.model.responsereport;
 
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
 public class ResponsePostReport {
+
+    @SerializedName("code")
+    @Expose
+    private String code;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
 }
